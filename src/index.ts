fe464cad7708b0acccd8bcfb84ca9ee1af1ad5f5
Jae Lo Presti (j4) <git@777.tf>
2023-10-20 13:25:19 +0300

    index: add graceful shutdown support

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fastify from 'fastify'
 import { join } from 'path'
 import { HOST, PRODUCTION } from './environment';
 import fastifyAutoload from '@fastify/autoload';
+import fastifyGracefulShutdown from 'fastify-graceful-shutdown';
 
 const server = fastify({
     logger: !PRODUCTION,
@@ -12,10 +13,19 @@ void server.register(fastifyAutoload, {
     dir: join(__dirname, 'routes'),
 });
 
+void server.register(fastifyGracefulShutdown);
+
 server.listen({ port: 8080, host: HOST }, (err, address) => {
     if(err) {
         console.error(err);
         process.exit(1);
     }
     console.log(`Server is listening on ${address}`);
+});
+
+server.after(() => {
+    server.gracefulShutdown((signal, next) => {
+        console.log('Shutting down.');
+        next();
+    });
 });
