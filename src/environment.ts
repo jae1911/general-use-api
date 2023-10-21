43017ea109236e536079da4fa56c33ceee1b7b53
Jae Lo Presti (j4) <git@777.tf>
2023-10-21 16:39:29 +0300

    main: add meta/version endpoint

--- src/environment.ts
+++ src/environment.ts
@@ -6,5 +6,6 @@ dotenv.config();
 
 export const PRODUCTION: boolean = env.NODE_ENV === 'production';
 export const HOST: string = env.HOST ?? '::';
+export const NODE: string = env.NODENAME ?? 'unknown';
 
 export const LIBRETRANSLATE_DOMAIN: string = env.LBRTRS_DOMAIN ?? '';
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,9 +1,19 @@
 import { FastifyPluginCallback } from 'fastify';
 
+import { NODE } from '../environment';
+
 const plugin: FastifyPluginCallback = (fastify, opts, next): void => {
 
-    fastify.get('/', async (req, res) => {
+    fastify.get('/', async (_req, res) => {
         await res.send('ok');
+    });
+
+    fastify.get('/meta/version', async (_req, res) => {
+        await res.send({
+            'name': 'gapi',
+            'node': NODE,
+            'version': 1,
+        });
     });
 
     next();
